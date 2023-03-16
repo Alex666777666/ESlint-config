fa0325ea2112b6a4feba1352e04e878eb1c48ddb
Alex <111201814+Alex666777666@users.noreply.github.com>
2023-03-16 15:13:20 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,21 +1,21 @@
 // file path: src/index.js
 
-import printProfile from './renderProfile'
+import printProfile from './renderProfile';
 
 const userData = {
   name: 'Tom',
   age: 17,
-}
+};
 
 const profile = {
   ...userData,
   company: 'Gromcode',
-}
+};
 
-printProfile(profile)
+printProfile(profile);
 
-const num = 17
+const num = 17;
 
-if (num == 18) {
-  alert('Hi') // eslint-disable-line no-alert
+if (num === 18) {
+  alert('Hi'); // eslint-disable-line no-alert
 }
